f77da5f55eafbcfe465309992aef11b03ca7ad19
CLayton Wakefield <acidduckling@gmail.com>
2018-06-13 16:28:27 +1000

    ngClass

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,13 +4,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   selector: 'app-cart',
   template: `
   <input [(ngModel)]="newItem" type="text">
-  <button type="button" class="btn btn-primary" (click)="addNewItem()">Add Item</button>
+  <button type="button" class="btn"
+    [ngClass]="{'btn-primary': newItem != '', 'btn-default': newItem === ''}"
+    (click)="addNewItem()">Add Item</button>
+
+  <!-- Iterating over items -->
+  <ul class="list-group">
+    <li class="list-group-item" *ngFor="let item of items">{{ item }}</li>
+  </ul>
   `,
   styles: []
 })
 export class CartComponent {
   @Input() items = [];
-  newItem: string;
+  newItem = '';
   @Output() itemAdded = new EventEmitter();
 
   addNewItem() {
